d398cc710964bfcf04710f012ecb2b90a6c96ef1
RohiniKhandare98 <rohini.khandare16598@gmail.com>
2026-03-14 17:38:48 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main 
+import "fmt"
+func main(){
+			// fmt.Println("ganapati bappa morya")
+			
+			name := "api-server"
+			port := 8080
+			isHealthy := true
+			
+			
+			// explicit type
+			var timeout int =30 
+			
+			// without any value
+			var retries int 
+			var label string 
+			var active bool
+			
+			fmt.Println(name , port , isHealthy ,timeout , retries , label , active)
+			
+			
+			
+}
